f461553169dcd7f4b5b24dce3fd7952eab30dbf8
Céline <celine.metz@outlook.com>
2024-04-26 12:30:08 +0200

    load info game

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Screenshots from '../components/info_game/Screenshots';
 import Trailer from '../components/info_game/Trailer';
+import Platform from '../components/info_game/Platform';
 
 function GamePage() {
     const { id } = useParams();
@@ -17,18 +18,53 @@ function GamePage() {
         .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    // TODO trailer : games / {id} / movies
+    console.log(gameInfo.metacritic_platforms);
 
     return (
         <>
         <Header />
         <main>
-            <section className="container">
+            <section className="container container-game-page">
                 <h1>{gameInfo.name}</h1>
                 <div className="game-images">
-                    <Screenshots id={id} />
                     <Trailer id={id} />
+                    <Screenshots id={id} />
+                </div>
+
+                <div className="game-info-important">
+                    <ul className="game-platform">
+                        {gameInfo.parent_platforms.map(platform => (
+                            <Platform platform={platform} key={platform.id} />
+                        ))}
+                    </ul>
+
+                    <ul className="game-stores">
+                        {gameInfo.stores.map(store => (
+                            <li key={store.store.id}>
+                                <a href={`http://${store.store.domain}`}>{store.store.name}</a>
+                            </li>
+                        ))}
+                    </ul>
                     
+                    <ul className="game-genres">
+                        {gameInfo.genres.map(genre => (
+                            <li key={genre.id}>
+                                <p className="game-genres-item">{genre.name}</p>
+                            </li>
+                        ))}
+                    </ul>
+
+                        {/* TODO : finish here (link + rating + name of website) */}
+                    {/* <ul className="game-metacritics">
+                        {gameInfo.metacritic_platforms.map(meta => (
+                            <li key={meta.id}>
+                                <a href={`http://${meta.platform.url}`}>{meta.platform.name}</a>
+                            </li>
+                        ))}
+                    </ul> */}
+
+                    <p>Rating: {gameInfo.rating}</p>
+
                 </div>
             </section>
         </main>
