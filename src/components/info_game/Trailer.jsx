8bf0b7731c8bc495408c49d92a3b14090ff79541
Céline <celine.metz@outlook.com>
2024-05-06 16:40:23 +0200

    fix render game page

--- src/components/info_game/Trailer.jsx
+++ src/components/info_game/Trailer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-export default function Trailer({ id, autoplay }) {
+export default function Trailer({ id, autoplay, handleZoom, resetZoom, zoomedImage }) {
     const [gameMovies, setGameMovies] = useState([]);
 
     useEffect(() => {
@@ -10,12 +10,32 @@ export default function Trailer({ id, autoplay }) {
         .catch(error => console.error('Error fetching data:', error));
     }, [id]); // Ajout de l'ID comme dépendance
 
+    console.log(gameMovies)
+
     if (gameMovies.length > 0) {
         return (
-            <div className="game-movies">
-                {gameMovies.map(movies => (
-                    <video key={movies.id} src={movies.data.max} controls autoPlay={autoplay} muted loop={autoplay} alt={movies.id} />
-                ))}
+            <div className="container-movies">
+                <div className="game-movies">
+                    {gameMovies.map(movies => (
+                        <video 
+                            onClick={() => handleZoom(screenshot)} 
+                            key={movies.id} 
+                            src={movies.data.max} 
+                            controls 
+                            autoPlay={autoplay} 
+                            muted 
+                            loop={autoplay} 
+                            alt={movies.id} />
+                    ))}
+                </div>
+                {zoomedImage && (
+                <img 
+                    onClick={resetZoom} 
+                    src={zoomedImage.image} 
+                    alt={zoomedImage.id} 
+                    style={{transform: 'scale(2)', transition: 'transform 0.3s ease-in-out'}} // Apply zoom effect
+                />
+            )}
             </div>
         );
     } else return null;
