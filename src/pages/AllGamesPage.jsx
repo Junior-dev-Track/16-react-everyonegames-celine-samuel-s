29193ed36a893e497e08ba61c27bd011c31f1d5d
SamuelSanchezGit <samuelsanchezsam@proton.me>
2024-05-07 14:16:32 +0200

    triParGenre

--- src/pages/AllGamesPage.jsx
+++ src/pages/AllGamesPage.jsx
@@ -5,11 +5,12 @@ import GameCard from '../components/GameCard';
 
 function AllGamesPage() {
     const [games, setGames] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [selectedGenre, setSelectedGenre] = useState('');
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
     const [currentLetter, setCurrentLetter] = useState('');
-    const [ordering, setOrdering] = useState('');
     const gameIds = useRef(new Set()); // Ensemble pour suivre les identifiants uniques
 
     // Charger les jeux par page
@@ -26,11 +27,11 @@ function AllGamesPage() {
 
         // Construire l'URL de la requête avec les paramètres nécessaires
         let url = `https://api.rawg.io/api/games?key=${apiKey}&page=${page}`;
-        if (ordering) {
-            url += `&ordering=${ordering}`;
-        }
         if (currentLetter) {
             url += `&search=${currentLetter}`;
+        }
+        if (selectedGenre) {
+            url += `&genres=${selectedGenre}`;
         }
 
         console.log("Fetching URL:", url);
@@ -52,11 +53,8 @@ function AllGamesPage() {
                 // Vérifiez que les propriétés essentielles existent
                 const hasValidProperties = game.name && game.background_image && game.parent_platforms;
 
-                // Filtrer par lettre initiale
-                const startsWithCurrentLetter = currentLetter ? game.name.toUpperCase().startsWith(currentLetter) : true;
-
-                // Ajoutez le jeu uniquement s'il est unique, valide et commence par la lettre choisie
-                return isUnique && hasValidProperties && startsWithCurrentLetter;
+                // Ajoutez le jeu uniquement s'il est unique et valide
+                return isUnique && hasValidProperties;
             });
 
             // Ajouter les identifiants uniques au `Set`
@@ -83,16 +81,38 @@ function AllGamesPage() {
         setHasMore(true);
         gameIds.current.clear(); // Réinitialiser le suivi des identifiants uniques
         loadGames(); // Charger la première page
-    }, [ordering, currentLetter]);
+    }, [currentLetter, selectedGenre]);
+
+    // Charger les genres disponibles
+    useEffect(() => {
+        const loadGenres = async () => {
+            const apiKey = import.meta.env.VITE_REACT_APP_RAWG_API_KEY;
+            const url = `https://api.rawg.io/api/genres?key=${apiKey}`;
+
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Erreur HTTP : ${response.status}`);
+                }
+
+                const data = await response.json();
+                setGenres(data.results);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des genres :', error.message);
+            }
+        };
+
+        loadGenres();
+    }, []);
 
     // Gestionnaire d'événement pour la sélection de la lettre
     const handleLetterChange = (event) => {
         setCurrentLetter(event.target.value);
     };
 
-    // Gestionnaire d'événement pour la sélection du tri
-    const handleOrderingChange = (event) => {
-        setOrdering(event.target.value);
+    // Gestionnaire d'événement pour la sélection du genre
+    const handleGenreChange = (event) => {
+        setSelectedGenre(event.target.value);
     };
 
     // Gestionnaire d'événement pour charger la page suivante lors du défilement
@@ -113,7 +133,7 @@ function AllGamesPage() {
             <main>
                 <section>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <h1>All games</h1>
+                        <h1>Tous les Jeux</h1>
                         <div>
                             <label htmlFor="letter">Filtrer par Lettre : </label>
                             <select onChange={handleLetterChange} value={currentLetter} aria-label="Filtrer par Lettre">
@@ -122,10 +142,12 @@ function AllGamesPage() {
                                     <option key={letter} value={letter}>{letter}</option>
                                 ))}
                             </select>
-                            <select onChange={handleOrderingChange} value={ordering} aria-label="Filtrer par">
-                                <option value="">Par défaut</option>
-                                <option value="slug">Nom (A-Z)</option>
-                                <option value="-slug">Nom (Z-A)</option>
+                            <label htmlFor="genre">Filtrer par Genre : </label>
+                            <select onChange={handleGenreChange} value={selectedGenre} aria-label="Filtrer par Genre">
+                                <option value="">Tous</option>
+                                {genres.map(genre => (
+                                    <option key={genre.id} value={genre.id}>{genre.name}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
